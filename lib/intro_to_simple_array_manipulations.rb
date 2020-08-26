2c92c4b1056f1035baf9217ecd8a711f62094a74
John Lovitsch <jmlovitsch@gmail.com>
2020-08-26 15:10:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,5 @@ end
 
 def using_shift_with_args (ice_cream_brands)
   ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  rands_removed = ice_cream_brands.shift(2)
+  brands_removed = ice_cream_brands.shift(2)
 end
