bc03f9e58d9ca0d07e06bd7ad8513566ecd800b4
John Lovitsch <jmlovitsch@gmail.com>
2020-08-26 15:16:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,8 +25,5 @@ def using_shift (my_favorite_cities)
   im_so_over_this_city = my_favorite_cities.shift
 end
 
-def using_shift_with_args (array)
-  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  brands_removed = ice_cream_brands.shift(2)
- 
-end
+def using_shift_with_args (ice_cream_brands)
+  []
